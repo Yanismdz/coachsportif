c62e252e87f6c1237b03327716e4b11c40ad3c0e
yanismdz <maoucha.yanis@gmail.com>
2016-07-31 15:11:50 +0200

    finish the 3 pages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+$(window).scroll(function()
+			    {
+				var fromTopPx = 50; // distance a parcourir pour que la bar change de couleur
+				var scrolledFromtop = $(window).scrollTop();
+				if(scrolledFromtop > fromTopPx)
+				{
+				    $(".top-bar, .menu").addClass("scrolled");
+				}
+				else
+				{
+				    $(".top-bar, .menu").removeClass("scrolled");
+				}
+			    });
