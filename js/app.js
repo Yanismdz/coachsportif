9e3cf73134dae6d5f971789352b14608f0e9ab11
yanismdz <maoucha.yanis@gmail.com>
2016-08-02 08:33:26 +0200

    add animation in app.js + add calendar in planning.html

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 $(document).foundation()
 
-$(window).scroll(function()
-			    {
-				var fromTopPx = 50; // distance a parcourir pour que la bar change de couleur
-				var scrolledFromtop = $(window).scrollTop();
-				if(scrolledFromtop > fromTopPx)
-				{
-				    $(".top-bar, .menu").addClass("scrolled");
+$(window).scroll(function(){
+    var fromTopPx = 50; // distance a parcourir pour que la bar change de couleur
+    var scrolledFromtop = $(window).scrollTop();
+    if(scrolledFromtop > fromTopPx)
+    {
+	$(".top-bar, .menu").addClass("scrolled");
 				}
-				else
-				{
-				    $(".top-bar, .menu").removeClass("scrolled");
-				}
-			    });
+    else
+    {
+	$(".top-bar, .menu").removeClass("scrolled");
+    }
+});
+
+$(document).ready(function(){
+    // Fade in au chargement de la page
+    $("body").css("display", "none");
+    $("body").fadeIn(400);
+    // Désactivation du lien pour lancer le fade out avant de rediriger
+    $('.globallink').click(function(e){
+	redirect = $(this).attr('href');
+	e.preventDefault();
+	$('body').fadeOut(400, function(){
+	    document.location.href = redirect
+	});
+    });
+});
